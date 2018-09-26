bf1d88ced66d8f13c760b1b2e520df895f9d6f74
Kristoffer Kvamme <33118819+KrisKvam@users.noreply.github.com>
2018-09-26 14:54:48 +0200

    FirstMaster

--- ComtechWinnerGetPriceFromPdf/Program.cs
+++ ComtechWinnerGetPriceFromPdf/Program.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
 
 namespace ComtechWinnerGetPriceFromPdf
 {
@@ -10,12 +10,64 @@ namespace ComtechWinnerGetPriceFromPdf
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            DirectoryInfo di = new DirectoryInfo(@"C:\GET\GET-Academy Solutions\Comtech Winner pris beregner");
+            FileInfo[] files = di.GetFiles("Tilbud-1.pdf", SearchOption.AllDirectories);
+            var totalPrice = 0;
+            foreach (var file in files)
+            {
+                Console.WriteLine(file.DirectoryName + "\\" + file.Name);
+                PdfReader pdfReader = new PdfReader(file.DirectoryName + "\\" + file.Name);
+                ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                string currentText = PdfTextExtractor.GetTextFromPage(pdfReader, pdfReader.NumberOfPages, strategy);
+                totalPrice += GetPriceFromText(currentText);
+            }
+            Console.WriteLine(totalPrice);
+        }
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        private static int GetPriceFromText(string currentText)
+        {
+            var firstSplit = currentText.Split(' ');
+            var wordsCorrect = 0;
+            var secondPartOfPrice = false;
+            var price = "";
+            var words = SplitOnNewLine(firstSplit);
+            foreach (var word in words)
+            {
+                if (wordsCorrect == 4 || secondPartOfPrice)
+                {
+                    secondPartOfPrice = !secondPartOfPrice;
+                    price += word;
+                }
+
+                if (word == "Totalt"
+                    || word == "ink"
+                    || word == "MVA"
+                    || word == "NOK") wordsCorrect++;
+                else wordsCorrect = 0;
+            }
+
+            Console.WriteLine(price);
+            if (price == "") price = "0";
+            var roundedPrice = price.Split(',');
+            return int.Parse(roundedPrice[0]);
+        }
+
+        private static string[] SplitOnNewLine(string[] firstSplit)
+        {
+            var words = new List<string>();
+            foreach (var s in firstSplit)
+            {
+                string[] lines = s.Split(
+                    new[] { "\r\n", "\r", "\n" },
+                    StringSplitOptions.None
+                );
+                foreach (var line in lines)
+                {
+                    words.Add(line);
+                }
+            }
+
+            return words.ToArray();
         }
     }
 }
